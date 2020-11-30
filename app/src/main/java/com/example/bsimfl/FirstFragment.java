0d52e59352233358c2c9874410a356249a052957
rurentero <46007399+rurentero@users.noreply.github.com>
2020-11-30 15:06:11 +0100

    Incluidos los datos. Creado un metodo para leer CSV. Falta probar.

--- app/src/main/java/com/example/bsimfl/FirstFragment.java
+++ app/src/main/java/com/example/bsimfl/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.bsimfl;
 
+import android.content.res.AssetManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +12,10 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.InputStreamInputSplit;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.BackpropType;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
@@ -20,12 +25,26 @@ import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.learning.config.Sgd;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class FirstFragment extends Fragment {
+
+    private final String dataFile = "a7ddfb7f-c221-4d5b-a5c2-9f5e289269e1.csv";
+    private final String file_x_train = "data/x_train/" + dataFile;
+    private final String file_x_test = "data/x_test/" + dataFile;
+    private final String file_y_train = "data/y_train/" + dataFile;
+    private final String file_y_test = "data/y_test/" + dataFile;
+    int batchSize = 20 ;
+    DataSet trainingData;
+    DataSet testData;
 
     @Override
     public View onCreateView(
@@ -51,7 +70,18 @@ public class FirstFragment extends Fragment {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-                createAndUseNetwork();
+                // TODO Probar la carga de datos
+                try {
+                    createDataSource(file_x_train);
+                } catch (IOException e) {
+                    Log.e("CARGA", "Algo ha ido mal en la carga de datos.");
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    Log.e("CARGA", "Algo ha ido mal en la carga de datos.");
+                    e.printStackTrace();
+                }
+                // TODO probar la creación de la red
+                //createAndUseNetwork();
             }
         });
     }
@@ -98,7 +128,7 @@ public class FirstFragment extends Fragment {
                 .layer(1, hiddenLayer)
                 .layer(2, outputLayer)
                 .build();
-
+        // TODO Como activar la backpropagation?
         // Model
         MultiLayerNetwork model = new MultiLayerNetwork(multiLayerConf);
         model.init();
@@ -107,26 +137,65 @@ public class FirstFragment extends Fragment {
         Log.i("NETWORK", "createAndUseNetwork: " + model.summary());
     }
 
-    // TODO Leer el CSV y devolver lista de arrays (contenido de cada columna)
-    private static void get_train_data() {
-        List<Integer[]> questionList = new ArrayList<String[]>();
-        AssetManager assetManager = context.getAssets();
+    // TODO CSV con CSVwriter
+    private void createDataSource(String dataFile) throws IOException, InterruptedException {
+        //Pre: Create input stream
+        Log.i("createDataSource: ", "0) Creando input stream");
+        AssetManager am = this.getContext().getAssets();
+        InputStream is = am.open(dataFile);
 
-        try {
-            InputStream csvStream = assetManager.open(CSV_PATH);
-            InputStreamReader csvStreamReader = new InputStreamReader(csvStream);
-            CSVReader csvReader = new CSVReader(csvStreamReader);
-            String[] line;
+        //First: get the dataset using the record reader. CSVRecordReader handles loading/parsing
+        Log.i("createDataSource: ", "1) Creando record reader");
+        int numLinesToSkip = 1; // Cabecera
+        char delimiter = ',';
+        RecordReader recordReader = new CSVRecordReader(numLinesToSkip, delimiter);
+        recordReader.initialize(new InputStreamInputSplit(is));
 
-            // throw away the header
-            csvReader.readNext();
+        //Second: the RecordReaderDataSetIterator handles conversion to DataSet objects, ready for use in neural network
+        int labelIndex = 5;
 
-            while ((line = csvReader.readNext()) != null) {
-                questionList.add(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return questionList;
+        DataSetIterator iterator = new RecordReaderDataSetIterator(recordReader, batchSize, labelIndex, labelIndex, true);
+        DataSet allData = iterator.next();
+
+        // Dividir en train test
+        SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.50);  //Use 50% of data for training
+
+        trainingData = testAndTrain.getTrain();
+        testData = testAndTrain.getTest();
+
+        // TODO Comprobar y borrar luego
+        Log.i("createDataSource: ", "DATASET DE ENTRENAMIENTO: " + trainingData.toString());
+        Log.i("createDataSource: ", "DATASET DE TESTEO: " + testData.toString());
+
+        // En principio no haria falta normalizar, ya se hizo en el preprocesado en Python.
+        //We need to normalize our data. We'll use NormalizeStandardize (which gives us mean 0, unit variance):
+//        DataNormalization normalizer = new NormalizerStandardize();
+//        normalizer.fit(trainingData);           //Collect the statistics (mean/stdev) from the training data. This does not modify the input data
+//        normalizer.transform(trainingData);     //Apply normalization to the training data
+//        normalizer.transform(testData);         //Apply normalization to the test data. This is using statistics calculated from the *training* set
     }
+
+
+    // TODO APLAZADO Leer el CSV y devolver lista de arrays (contenido de cada columna)
+//    private static void get_train_data() {
+//        List<Integer[]> questionList = new ArrayList<String[]>();
+//        AssetManager assetManager = context.getAssets();
+//
+//        try {
+//            InputStream csvStream = assetManager.open(CSV_PATH);
+//            InputStreamReader csvStreamReader = new InputStreamReader(csvStream);
+//            CSVReader csvReader = new CSVReader(csvStreamReader);
+//            String[] line;
+//
+//            // throw away the header
+//            csvReader.readNext();
+//
+//            while ((line = csvReader.readNext()) != null) {
+//                questionList.add(line);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return questionList;
+//    }
 }
